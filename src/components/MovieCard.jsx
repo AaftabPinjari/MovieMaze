8a4d5928960c05410b65c901723e6a8dca223cc5
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2024-03-30 14:36:15 +0530

    forgot to save

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -13,7 +13,7 @@ function MovieCard({ movie }) {
                 className="h-[300px] w-[200px] rounded-lg"
                 src={movie.Poster} />
             <h1 className="text-xl font-serif truncate w-full text-center">{movie.Title}</h1>
-            <h3 className="text-xl font-serif  w-full text-center">{movie.Year}</h3>
+            <h3 className="text-xl font-serif w-full text-center">{movie.Year}</h3>
             <div className="flex justify-between gap-4 font-semibold mt-3">
                 <Link
                     className=" border-[1px] px-6 py-1 rounded bg-white text-black  "
